55c36e6029735fe55c2d1d302e1db3cc76f8d699
Aliaksandr <Nicepk.by@gmail.com>
2021-07-14 19:51:03 +0200

    dsad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+#https://api.telegram.org/bot1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M/setWebhook?url=https://belle-gerard-08161.herokuapp.com/
 
 TOKEN = '1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M'
 URL = 'https://api.telegram.org/bot' + TOKEN + '/'
@@ -25,15 +26,18 @@ def send_message(chat_id, text='Введите лошин'):
     requests.get(url)
 
 def main():
-    #d = get_updates()
-   # with open('update.json', 'w') as file:
-    #    json.dump(d, file, indent=2, ensure_ascii=False)
     answer = get_message()
     chat_id = answer['chat_id']
     text = answer['text']
 
     if '/Start' in text:
+        send_message(chat_id, 'Введите логин')
+
+    if 'Login' in text:
         send_message(chat_id, 'Введите пароль')
+
+    if 'Pass' in text:
+        send_message(chat_id, 'ok')
 
     
 if __name__ == '__main__':
