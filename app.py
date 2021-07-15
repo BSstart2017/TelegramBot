f64304e644112ed5d8453cbf52d1484baffc11ba
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 10:39:44 +0200

    kom10

--- app.py
+++ app.py
@@ -1,53 +1,33 @@
-from flask import Flask, app, request
+from flask import Flask, app, request, jsonify
 import requests
-
+import json
 
 app = Flask(__name__)
-#https://api.telegram.org/bot1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M/setWebhook?url=https://belle-gerard-08161.herokuapp.com/
 
-TOKEN = '1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M'
-URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+URL = 'https://api.telegram.org/bot1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M/'
 
-def get_updates():
-    url = URL + 'getupdates'
-    r = requests.get(url)
-    return r.json()
-
-def get_message():
-    data = get_updates()
-    
-    chat_id = data['result'][-1]['message']['chat']['id']
-    message_text = data['result'][-1]['message']['text']
-
-    message = {'chat_id': chat_id,
-               'text': message_text}
-
-    return message
+def write_json(data, filename='answer.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 def send_message(chat_id, text='Введите лошин'):
-    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
-    requests.get(url)
-
-def main():
-    answer = get_message()
-    chat_id = answer['chat_id']
-    text = answer['text']
-
-    if '/Start' in text:
-        send_message(chat_id, 'Введите логин')
-
-    if 'Login' in text:
-        send_message(chat_id, 'Введите пароль')
-
-    if 'Pass' in text:
-        send_message(chat_id, 'ok')
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url, json=answer)
+    return r.json()
 
 @app.route("/", methods=["GET", "POST"])
-def receive_update():
+def index():
     if request.method == "POST":
-        print(request.json)
-    return {"ok": True}
+        r = request.get_json()
+        chat_id = r['message']['chat']['id']
+        message = r['message']['text']
 
-    
+        if '/Start' in message:
+            send_message(chat_id, text='Введите логин')
+
+        return jsonify(r)
+    return '<h1>privetik</h1>'
+
 if __name__ == '__app__':
     app.run()
---
+++ parse.py
@@ -0,0 +1,10 @@
+import requests
+from app import write_json
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
