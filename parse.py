05c0d240ca304f2b01d0bfeaee01efc66bddc355
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 11:33:34 +0200

    kom10

--- parse.py
+++ parse.py
@@ -1,10 +1,17 @@
-import requests
-from app import write_json
+import telebot
+bot = telebot.TeleBot('1717938340:AAFr7fwkyg94ZWi0vOcJ-yErL8JB4NzfN4M')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
 
 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
-def main():
-    pass
 
-if __name__ == '__main__':
-    main()
+bot.polling(none_stop=True)
